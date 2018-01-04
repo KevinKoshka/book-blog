b8f464373b05c4b770a8b1fa4891466ee1e91f91
kevin.coscarelli <kevin.coscarelli@globant.com>
2018-01-04 17:01:45 -0300

    19. Sidebar full con estilos, article con estilos. Tratando de arreglar el layout.

--- src/app/navigator/navigator.component.ts
+++ src/app/navigator/navigator.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Book } from '../book';
 import { UtilitiesService } from '../utilities.service';
+import { ObService } from '../ob-service.service';
+import { Article } from '../article';
 
 @Component({
   selector: 'app-navigator',
   templateUrl: './navigator.component.html',
   styleUrls: ['./navigator.component.scss']
 })
+
 export class NavigatorComponent implements OnInit {
   @Input() books: Book[];
+
+  subscription: Subscription;
+
+  current = {
+    bookname: '',
+    chaptername: '',
+    pagename: ''
+  }
 
   makeID(book, chapter?, page?) {
     book = this.util.idify(book);
@@ -51,7 +63,18 @@ export class NavigatorComponent implements OnInit {
     }
   }
 
-  constructor(private util: UtilitiesService) { }
+  constructor(
+    private util: UtilitiesService,
+    private obService: ObService
+  ) {
+    this.obService.clickedPage$.subscribe(
+      article => {
+        this.current.bookname = util.idify(article.title);
+        this.current.chaptername = util.idify(article.chapter);
+        this.current.pagename = util.idify(article.subtitle);
+      }
+    )
+  }
 
   ngOnInit() {
   }
